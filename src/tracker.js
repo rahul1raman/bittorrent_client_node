9857b19d64b3c95526daab46cee2e0d74040b22e
Rahul Raman <rahul1raman@gmail.com>
2023-09-10 16:46:18 +0530

    Add tracker request and response functions

--- src/tracker.js
+++ src/tracker.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { createSocket } = require('dgram');
+const { randomBytes } = require('crypto');
 
 module.exports.getPeers = async (torrent, callback) => {
   const socket = createSocket('udp4');
@@ -8,12 +9,12 @@ module.exports.getPeers = async (torrent, callback) => {
   console.log('Getting peers for: ', url);
 
   // 1. send connect request
-  await udpSend(socket, buildConnReq(), url);
+  await udpSend(socket, buildRequestBuffer(), url);
 
   socket.on('message', async response => {
     if (respType(response) === 'connect') {
       // 2. receive and parse connect response
-      const connResp = parseConnResp(response);
+      const connResp = parseResponse(response);
       // 3. send announce request
       const announceReq = buildAnnounceReq(connResp.connectionId);
       await udpSend(socket, announceReq, url);
@@ -35,12 +36,31 @@ function respType(resp) {
   // ...
 }
 
-function buildConnReq() {
-  // ...
+function buildRequestBuffer() {
+  // build connection req based on BEP format: 
+  // http://www.bittorrent.org/beps/bep_0015.html
+
+  const buf = Buffer.alloc(16); // message should be 16 bytes long
+
+  // connection id
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+
+  // action
+  buf.writeUInt32BE(0, 8);
+
+  // random transaction id
+  randomBytes(4).copy(buf, 12);
+
+  return buf;
 }
 
-function parseConnResp(resp) {
-  // ...
+function parseResponse(resp) {
+  return {
+    action: resp.readUint32BE(0),
+    transactionId: resp.readUint32BE(4),
+    connectionId: resp.slice(8),
+  }
 }
 
 function buildAnnounceReq(connId) {
