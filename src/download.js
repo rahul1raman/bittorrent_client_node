ac7d60d3c5e46122504469e7fc7b7b9a9ec2c062
Rahul Raman <rahul1raman@gmail.com>
2023-09-11 23:49:47 +0530

    Add onWholeMsg

--- src/download.js
+++ src/download.js
@@ -11,10 +11,29 @@ function downloadFromPeer(peer) {
 
     });
 
-    socket.on('data', data => {
+    onWholeMsg(socket, data => {
 
     });
 }
+
+
+function onWholeMsg(socket, callback) {
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+        // msgLen calculates the length of a whole message
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+    
+        while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+          callback(savedBuf.slice(0, msgLen()));
+          savedBuf = savedBuf.slice(msgLen());
+          handshake = false;
+        }
+    });
+}
+
 
 export const download = torrent => {
     tracker.getPeers(torrent, peers => {
