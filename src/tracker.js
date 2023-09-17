deb75b09739793adef46133cb284cca1efc324d5
Rahul Raman <rahul1raman@gmail.com>
2023-09-17 17:05:26 +0530

    Refactor tracker.js, moved some code to util

--- src/tracker.js
+++ src/tracker.js
@@ -1,39 +1,17 @@
-'use strict';
-import { createRequire } from 'module';
+"use strict";
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-const { createSocket } = require('dgram');
-const { randomBytes } = require('crypto');
-const axios = require('axios');
-const bignum = require('bignum');
+const { randomBytes } = require("crypto");
+const axios = require("axios");
+const bignum = require("bignum");
 
-import {genId} from './utils.js';
 
-export const getPeers = async (torrent) => {
-  const trackers = torrent.announce;
-  const peers = [];
-
-  for (const trackerUrl of trackers) {
-    console.log('Getting peers for: ', trackerUrl);
-    // if (trackerUrl.startsWith('http')) {
-    //   const httpPeers = await getPeersFromHttpTracker(trackerUrl, torrent);
-    //   peers.push(...httpPeers);
-    // } else 
-    if (trackerUrl.startsWith('udp')) {
-      const connection = await getConnection(trackerUrl);
-      if (connection) {
-        const { peers: udpPeers } = await getPeersFromUDPTracker(trackerUrl, torrent, connection.connectionId);
-        peers.push(...udpPeers);
-      }
-    }
-  }
-
-  return peers;
-};
+import { genId, isCompactFormat, udpSend, deduplicateByKey } from "./utils.js";
 
 
 export function buildConnectionMessage() {
-  // build connection req based on BEP format: 
+  // build connection req based on BEP format:
   // http://www.bittorrent.org/beps/bep_0015.html
 
   const buf = Buffer.alloc(16); // message should be 16 bytes long
@@ -56,33 +34,30 @@ function parseConnResponse(resp) {
     action: resp.readUint32BE(0),
     transactionId: resp.readUint32BE(4),
     connectionId: resp.slice(8),
-  }
+  };
 }
 
-function buildAnnounceReq(connectionId, torrent, port=6881) {
+function buildAnnounceReq(connectionId, torrent, port = 6881) {
   const buf = Buffer.alloc(98);
 
   // Connection ID
-  // buf.writeUInt32BE(0x417, 0);
-  // buf.writeUInt32BE(0x27101980, 4);
-
   connectionId.copy(buf, 0);
 
   // Action (announce)
   buf.writeUInt32BE(0x1, 8);
 
   // Transaction ID
-  randomBytes(4).copy(buf, 12)
+  randomBytes(4).copy(buf, 12);
 
   // Info Hash
-  Buffer.from(torrent.infoHash, 'hex').copy(buf, 16, 0, 20);
+  Buffer.from(torrent.infoHash, "hex").copy(buf, 16, 0, 20);
 
   // Peer ID
   genId().copy(buf, 36, 0, 20);
 
   buf.writeUInt32BE(0, 56); // downloaded
-  buf.writeUInt32BE(bignum.toBuffer(torrent.length, {size: 8}), 64); // left
-  buf.writeUInt32BE(0, 72);  // uploaded
+  buf.writeUInt32BE(bignum.toBuffer(torrent.length, { size: 8 }), 64); // left
+  buf.writeUInt32BE(0, 72); // uploaded
 
   // Event
   buf.writeInt32BE(0, 80); // event
@@ -91,7 +66,7 @@ function buildAnnounceReq(connectionId, torrent, port=6881) {
   buf.writeUInt32BE(0, 84);
 
   // Key
-  randomBytes(4).copy(buf, 88, 0, 4)
+  randomBytes(4).copy(buf, 88, 0, 4);
 
   // Num Want
   buf.writeInt32BE(-1, 92);
@@ -124,7 +99,7 @@ function parseAnnounceResp(responseBuffer) {
           const peerIPBytes = peersBuffer.slice(i, i + 4);
           const peerPortBytes = peersBuffer.slice(i + 4, i + 6);
 
-          const peerIP = Array.from(peerIPBytes).join('.');
+          const peerIP = Array.from(peerIPBytes).join(".");
           const peerPort = peerPortBytes.readUInt16BE(0);
 
           peers.push({ ip: peerIP, port: peerPort });
@@ -134,7 +109,7 @@ function parseAnnounceResp(responseBuffer) {
       // Handle non-compact format
       for (let i = 0; i < peersBuffer.length; i += 6) {
         if (i + 6 <= peersBuffer.length) {
-          const peerIP = peersBuffer.slice(i, i + 4).join('.');
+          const peerIP = peersBuffer.slice(i, i + 4).join(".");
           const peerPort = peersBuffer.readUInt16BE(i + 4);
           peers.push({ ip: peerIP, port: peerPort });
         }
@@ -150,7 +125,7 @@ function parseAnnounceResp(responseBuffer) {
       peers,
     };
   } catch (e) {
-    return { peers: []};
+    return { peers: [] };
   }
 }
 
@@ -165,8 +140,8 @@ async function getPeersFromHttpTracker(announceUrl, torrent) {
       left: torrent.length, // Amount left to download (initially equals total size)
       compact: 1, // Compact response format (1 for yes, 0 for no)
       numwant: 50, // Number of peers you want in the response
-      event: 'started' // Event type (started, stopped, completed)
-    }
+      event: "started", // Event type (started, stopped, completed)
+    },
   });
 
   const peers = parseAnnounceResp(response.data);
@@ -175,77 +150,31 @@ async function getPeersFromHttpTracker(announceUrl, torrent) {
 }
 
 
-async function getConnection(announceUrl) {
-  const trackerUrl = new URL(announceUrl);
-  const socket = createSocket('udp4');
-  let connection = null;
+export const getPeers = async (torrent) => {
+  const trackers = torrent.announce;
+  const peers = [];
 
-  return new Promise((resolve, reject) => {
-    socket.on('error', (err) => {
-      console.error('Error pinging UDP url: ', announceUrl);
-      // console.error(err);
-      resolve(null);
-      if (socket.listening) socket.close();
-    });
+  for (const trackerUrl of trackers) {
+    console.log("Getting peers for: ", trackerUrl);
+    // TODO: Make http tracker work
+    // if (trackerUrl.startsWith('http')) {
+    //   const httpPeers = await getPeersFromHttpTracker(trackerUrl, torrent);
+    //   peers.push(...httpPeers);
+    // } else
+    if (trackerUrl.startsWith("udp")) {
+      const message = buildConnectionMessage();
+      const connection = await udpSend(trackerUrl, message, parseConnResponse);
+      if (connection) {
+        const message = buildAnnounceReq(connection.connectionId, torrent);
+        const result = await udpSend(
+          trackerUrl,
+          message,
+          parseAnnounceResp
+        );
+        if (result) peers.push(...result?.peers);
+      }
+    }
+  }
 
-    socket.on('message', (msg) => {
-      connection = parseConnResponse(msg);
-      console.log('Connection found: ', connection);
-      resolve(connection);
-      if (socket.listening) socket.close();
-    });
-
-    const message = buildConnectionMessage();
-    socket.send(message, 0, message.length, trackerUrl.port, trackerUrl.hostname);
-
-    setTimeout(() => {
-      console.error('Timeout: No response from UDP endpoint', announceUrl);
-      resolve(null);
-      if (socket.listening) socket.close();
-      
-    }, 5000);
-  });
-}
-
-
-async function getPeersFromUDPTracker(announceUrl, torrent, connectionId) {
-  const trackerUrl = new URL(announceUrl);
-  const socket = createSocket('udp4');
-
-  let peers = {
-    peers: [],
-  };
-
-  return new Promise((resolve, reject) => {
-    socket.on('error', (err) => {
-      console.error('Error pinging UDP url: ', announceUrl);
-      // console.error(err);
-      resolve(peers);
-      if (socket.listening) socket.close();
-    });
-
-    socket.on('message', (msg) => {
-      peers = parseAnnounceResp(msg);
-      console.log('Peers found: ', peers);
-      resolve(peers);
-      if (socket.listening) socket.close();
-    });
-
-    // Send announce request
-    const message = buildAnnounceReq(connectionId, torrent);
-    socket.send(message, 0, message.length, trackerUrl.port, trackerUrl.hostname);
-
-    setTimeout(() => {
-      console.error('Timeout: No response from UDP endpoint', announceUrl);
-      resolve(peers);
-      if (socket.listening) socket.close();
-      
-    }, 5000);
-  });
-}
-
-
-function isCompactFormat(responseBuffer) {
-  // In compact format, the length of the peers section is a multiple of 6
-  return (responseBuffer.length - 20) % 6 === 0;
-}
+  return deduplicateByKey('ip', peers);
+};
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,60 @@
-'use strict';
+"use strict";
 
-import {randomBytes} from 'crypto';
-import * as dgram from 'dgram';
+import { randomBytes } from "crypto";
+import { createSocket } from "dgram";
 
-import { buildConnectionMessage } from './tracker.js';
+import { uniqBy, prop} from "ramda";
 
 let id = null;
 
+export const deduplicateByKey = (key, array) => uniqBy(prop(key), array);
+
 export const genId = () => {
-    // Used to generate peer id to uniquely identify the client
-    // "RT" is the name of my client (raman-torrent), and 0001 is the version number
-    if (!id) {
-        id = randomBytes(20);
-        Buffer.from('-RT0001-').copy(id, 0);
-    }
-    return id;
+  // Used to generate peer id to uniquely identify the client
+  // "RT" is the name of my client (raman-torrent), and 0001 is the version number
+  if (!id) {
+    id = randomBytes(20);
+    Buffer.from("-RT0001-").copy(id, 0);
+  }
+  return id;
+};
+
+export const isCompactFormat = (responseBuffer) => {
+    // In compact format, the length of the peers section is a multiple of 6
+    return (responseBuffer.length - 20) % 6 === 0;
+}
+
+export const udpSend = (rawUrl, message, messageParser) => {
+  const url = new URL(rawUrl);
+  const socket = createSocket("udp4");
+  let connection = null;
+
+  return new Promise((resolve) => {
+    socket.on("error", (err) => {
+      console.error("Error pinging UDP url: ", url);
+      if (socket.listening) socket.close();
+      resolve(null);
+    });
+
+    socket.on("message", (msg) => {
+      connection = messageParser(msg);
+      console.log("Connection found: ", connection);
+      resolve(connection);
+      if (socket.listening) socket.close();
+    });
+
+    socket.send(
+      message,
+      0,
+      message.length,
+      url.port,
+      url.hostname
+    );
+
+    setTimeout(() => {
+      console.error("Timeout: No response from UDP endpoint", url);
+      resolve(null);
+      if (socket.listening) socket.close();
+    }, 5000);
+  });
 };
